d8256c2d5c5440a4c184c0959544be622a606d2c
ninjascare <you@example.com>
2019-04-04 16:47:53 -0400

    created functionsd to handle the events that go with using a search bar

--- videos/src/components/SearchBar.js
+++ videos/src/components/SearchBar.js
@@ -1,13 +1,34 @@
 import React, { Component } from "react";
 
 export default class SearchBar extends Component {
+  state = {
+    term: ""
+  };
+
+  handleChange = e => {
+    console.log(e.target.value);
+    this.setState({
+      term: e.target.value
+    });
+  };
+
+  handleSubmit = eve => {
+    eve.preventDefault();
+    // TODO: make sure we call
+    // callback from parent Component
+  };
+
   render() {
     return (
       <div className="search-bar ui segment">
-        <form className="ui form">
+        <form onSubmit={this.handleSubmit} className="ui form">
           <div className="field">
             <label>Video Search</label>
-            <input type="text" />
+            <input
+              type="text"
+              value={this.state.term}
+              onChange={this.handleChange}
+            />
           </div>
         </form>
       </div>
