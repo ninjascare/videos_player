0fa52f55ee232cd2a4ac405481c8336bac3ff2aa
ninjascare <you@example.com>
2019-04-04 16:38:38 -0400

    added some more logic

--- videos/src/components/SearchBar.js
+++ videos/src/components/SearchBar.js
@@ -3,8 +3,13 @@ import React, { Component } from "react";
 export default class SearchBar extends Component {
   render() {
     return (
-      <div>
-        <h1>SearchBar</h1>
+      <div className="search-bar ui segment">
+        <form className="ui form">
+          <div className="field">
+            <label>Video Search</label>
+            <input type="text" />
+          </div>
+        </form>
       </div>
     );
   }
