668a8328283472f07d17fcba92b4c5e97f4dbfcb
ninjascare <you@example.com>
2019-04-08 12:08:57 -0400

    used the api to make calls to youtube to get actual data

--- videos/src/components/App.js
+++ videos/src/components/App.js
@@ -3,18 +3,28 @@ import SearchBar from "./SearchBar";
 import youtube from "../api/youtube";
 
 export default class App extends Component {
+  state = {
+    videos: []
+  };
+
   onTermSubmit = term => {
-    youtube.get("/search", {
-      params: {
-        q: term
-      }
-    });
+    youtube
+      .get("/search", {
+        params: {
+          q: term
+        }
+      })
+      .then(response => {
+        console.log(response.data.items);
+        this.setState({ videos: response.data.items });
+      });
   };
 
   render() {
     return (
       <div className="ui container">
         <SearchBar onTermSubmit={this.onTermSubmit} />
+        <h1>I have {this.state.videos.length} videos</h1>
       </div>
     );
   }
