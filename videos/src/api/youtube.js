1bc89049c00859c39091a1da55d229d50d4b0cae
ninjascare <you@example.com>
2019-04-08 11:02:17 -0400

    added a new folder and file for my youtube api so that way it can stay seperated

--- videos/src/api/youtube.js
+++ videos/src/api/youtube.js
@@ -1,1 +1,12 @@
+import axios from "axios";
+
 const KEY = "AIzaSyCZo4qdE5fyrd6HKyfOw2rLP-Uq20fEdpU";
+
+export default axios.create({
+  baseURL: "https://www.googleapis.com/youtube/v3",
+  params: {
+    part: "snippet",
+    maxResults: 5,
+    key: KEY
+  }
+});
